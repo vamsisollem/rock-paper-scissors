a7488df47a474408bd2f0ecb84a4af3f5968a360
Vamsi Sollem <vamsisollem@MacBook-Air-9.local>
2024-12-08 18:43:31 -0500

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      'darkText':'var(--Dark-Text)',
+      'score': 'var(--Score-Text)',
+      'outline':'var(--Header-Outline)',
+      'white':'var(--white)',
+      'circle': 'var(--circle)'
+    },
+    backgroundImage:{
+      'background':'var(--Radial-Gradient)',
+      'scissors':'var(--Scissors-Gradient)',
+      'paper':'var(--Paper-Gradient)',
+      'rock':'var(--Rock-Gradient)',
+      'triangle': 'var(--triangle)',
+    },
   },
   plugins: [],
 }
